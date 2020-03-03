bdd6c82d7aee7ce84767818e4d09db1cbc3fa8ea
innoflash <innocentmazando@gmail.com>
2020-03-03 09:11:10 +0200

    added many events trait

---
+++ src/Traits/HasManyAlbums.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace FaithGen\Gallery\Traits;
+
+
+use FaithGen\Gallery\Models\Album;
+
+trait HasManyAlbums
+{
+    /**
+     * Links many albums to a model
+     * @return mixed
+     */
+    public function albums()
+    {
+        return $this->hasMany(Album::class);
+    }
+}
