dc648e2b44f221acfed5dbe812f0afde9ac87408
innoflash <innocentmazando@gmail.com>
2020-06-20 04:45:43 +0200

    updated gallery routes

--- routes/gallery.php
+++ routes/gallery.php
@@ -4,8 +4,8 @@ use FaithGen\Gallery\Http\Controllers\AlbumController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('albums.')->prefix('albums/')->group(function () {
-    Route::get('/', [AlbumController::class, 'index']);
-    Route::get('view/{album}', [AlbumController::class, 'view']);
+    Route::get('', [AlbumController::class, 'index']);
+    Route::get('{album}', [AlbumController::class, 'view']);
     Route::get('comments/{album}', [AlbumController::class, 'comments']);
     Route::post('comment/{album}', [AlbumController::class, 'comment']);
 });
